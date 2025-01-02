f6852a87352398cbf3f04c1a17887b9fceb9bb62
daeho220 <daeho210@gmail.com>
2025-01-03 00:09:27 +0900

    Feat: app.module에 각 모듈들 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
-import { Module } from "@nestjs/common";
-import { DatabaseModule } from "./database/database.module";
-
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
+import { PointModule } from './point/point.module';
+import { CouponModule } from './coupon/coupon.module';
 @Module({
-  imports: [DatabaseModule],
-  controllers: [],
-  providers: [],
+    imports: [ProductModule, OrderModule, PaymentModule, PointModule, CouponModule],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
