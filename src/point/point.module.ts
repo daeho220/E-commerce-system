026d29cf6aeff563d6c5b6c0b74078b5814d8417
daeho220 <daeho210@gmail.com>
2025-01-10 02:53:57 +0900

    Delete: Point 모듈 삭제
    - User 모듈에서 기능 구현하는 것으로 변경

--- src/point/point.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PointsMockController } from './mock/point.mock.controller';
-
-@Module({
-    imports: [],
-    controllers: [],
-    providers: [],
-})
-export class PointModule {}
