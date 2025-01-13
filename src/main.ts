1a3e84cd30f097ac939ac4d929b276d6ee645184
daeho220 <daeho210@gmail.com>
2025-01-13 23:56:41 +0900

    Feat: httpExceptionFilter 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // 전역 필터 설정
+    app.useGlobalFilters(new HttpExceptionFilter());
 
     // 전역 인터셉터 설정
     // Winston Logger를 주입받아 Interceptor에 전달
