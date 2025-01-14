ade74a7b104c4f0a91690180c92b66dac9f8aa86
daeho220 <daeho210@gmail.com>
2025-01-15 01:07:55 +0900

    Fix: 단일 로그 파일로 변경 (이유: 추후 ELK 사용 예정)

--- src/configs/winston.config.ts
+++ src/configs/winston.config.ts
@@ -5,46 +5,25 @@ import 'winston-daily-rotate-file';
 export const winstonConfig = {
     transports: [
         new winston.transports.File({
-            filename: 'logs/error.log',
-            level: 'error',
-            format: winston.format.combine(
-                winston.format.timestamp({
-                    format: () =>
-                        new Date().toLocaleString('ko-KR', {
-                            timeZone: 'Asia/Seoul',
-                            year: 'numeric',
-                            month: '2-digit',
-                            day: '2-digit',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            second: '2-digit',
-                            fractionalSecondDigits: 3,
-                        }),
-                }),
-                winston.format.json(),
-                winston.format.prettyPrint(),
-            ),
-        }),
-        new winston.transports.File({
-            filename: 'logs/httpLog.log',
-            format: winston.format.combine(
-                winston.format.timestamp({
-                    format: () =>
-                        new Date().toLocaleString('ko-KR', {
-                            timeZone: 'Asia/Seoul',
-                            year: 'numeric',
-                            month: '2-digit',
-                            day: '2-digit',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            second: '2-digit',
-                            fractionalSecondDigits: 3,
-                        }),
-                }),
-                winston.format.json(),
-                winston.format.prettyPrint(),
-            ),
+            filename: 'logs/application.log',
             level: 'http',
+            format: winston.format.combine(
+                winston.format.timestamp({
+                    format: () =>
+                        new Date().toLocaleString('ko-KR', {
+                            timeZone: 'Asia/Seoul',
+                            year: 'numeric',
+                            month: '2-digit',
+                            day: '2-digit',
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            second: '2-digit',
+                            fractionalSecondDigits: 3,
+                        }),
+                }),
+                winston.format.json(),
+                winston.format.prettyPrint(),
+            ),
         }),
     ],
 };
