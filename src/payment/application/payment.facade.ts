613e2b9b8ba0d749f941911c7eac565ca26b8108
daeho220 <daeho210@gmail.com>
2025-01-23 21:22:35 +0900

    Feat: 결제 생성 분산락 적용 / 락 성능 비교 테스트 제거

--- src/payment/application/payment.facade.ts
+++ src/payment/application/payment.facade.ts
@@ -30,73 +30,73 @@ export class PaymentFacade {
     ) {}
 
     // 비관적 락
-    async createPayment(dto: FacadeCreatePaymentDto): Promise<PrismaPayment> {
-        const facadeCreatePaymentDto = plainToInstance(FacadeCreatePaymentDto, dto);
+    // async createPayment(dto: FacadeCreatePaymentDto): Promise<PrismaPayment> {
+    //     const facadeCreatePaymentDto = plainToInstance(FacadeCreatePaymentDto, dto);
 
-        const errors = await validate(facadeCreatePaymentDto);
-        if (errors.length > 0) {
-            throw new BadRequestException('유효하지 않은 결제 데이터입니다.');
-        }
+    //     const errors = await validate(facadeCreatePaymentDto);
+    //     if (errors.length > 0) {
+    //         throw new BadRequestException('유효하지 않은 결제 데이터입니다.');
+    //     }
 
-        try {
-            return await this.prisma.$transaction(async (tx) => {
-                // 주문 조회
-                const order = await this.orderService.findByUserIdandOrderIdwithLock(
-                    facadeCreatePaymentDto.user_id,
-                    facadeCreatePaymentDto.order_id,
-                    tx,
-                );
+    //     try {
+    //         return await this.prisma.$transaction(async (tx) => {
+    //             // 주문 조회
+    //             const order = await this.orderService.findByUserIdandOrderIdwithLock(
+    //                 facadeCreatePaymentDto.user_id,
+    //                 facadeCreatePaymentDto.order_id,
+    //                 tx,
+    //             );
 
-                // 주문서 상태 검증
-                if (order.status !== OrderStatus.PENDING) {
-                    throw new BadRequestException(`주문서 상태가 ${order.status} 입니다.`);
-                }
+    //             // 주문서 상태 검증
+    //             if (order.status !== OrderStatus.PENDING) {
+    //                 throw new BadRequestException(`주문서 상태가 ${order.status} 입니다.`);
+    //             }
 
-                // 결제 생성
-                const payment = await this.paymentService.createPayment(facadeCreatePaymentDto, tx);
+    //             // 결제 생성
+    //             const payment = await this.paymentService.createPayment(facadeCreatePaymentDto, tx);
 
-                if (facadeCreatePaymentDto.payment_method === PaymentMethod.POINT) {
-                    // 사용자 잔액 차감
-                    await this.userService.useUserPoint(
-                        facadeCreatePaymentDto.user_id,
-                        order.total_price,
-                        tx,
-                    );
+    //             if (facadeCreatePaymentDto.payment_method === PaymentMethod.POINT) {
+    //                 // 사용자 잔액 차감
+    //                 await this.userService.useUserPoint(
+    //                     facadeCreatePaymentDto.user_id,
+    //                     order.total_price,
+    //                     tx,
+    //                 );
 
-                    // 포인트 사용 내역 생성
-                    await this.historyService.createPointHistory(
-                        facadeCreatePaymentDto.user_id,
-                        order.total_price,
-                        PointChangeType.USE,
-                        tx,
-                    );
-                } else if (facadeCreatePaymentDto.payment_method === PaymentMethod.CARD) {
-                    // 카드 결제
-                }
+    //                 // 포인트 사용 내역 생성
+    //                 await this.historyService.createPointHistory(
+    //                     facadeCreatePaymentDto.user_id,
+    //                     order.total_price,
+    //                     PointChangeType.USE,
+    //                     tx,
+    //                 );
+    //             } else if (facadeCreatePaymentDto.payment_method === PaymentMethod.CARD) {
+    //                 // 카드 결제
+    //             }
 
-                // 결제 상태 업데이트
-                await this.paymentService.updatePaymentStatus(payment.id, PaymentStatus.PAID, tx);
+    //             // 결제 상태 업데이트
+    //             await this.paymentService.updatePaymentStatus(payment.id, PaymentStatus.PAID, tx);
 
-                // 주문서 상태 업데이트
-                await this.orderService.updateOrderStatus(order.id, OrderStatus.PAID, tx);
+    //             // 주문서 상태 업데이트
+    //             await this.orderService.updateOrderStatus(order.id, OrderStatus.PAID, tx);
 
-                // 결제 정보 조회
-                const result = await this.paymentService.findPaymentByIdWithLock(payment.id, tx);
+    //             // 결제 정보 조회
+    //             const result = await this.paymentService.findPaymentByIdWithLock(payment.id, tx);
 
-                // 데이터 플랫폼 전송
-                const fakeDataPlatform = new FakeDataPlatform();
-                fakeDataPlatform.send();
+    //             // 데이터 플랫폼 전송
+    //             const fakeDataPlatform = new FakeDataPlatform();
+    //             fakeDataPlatform.send();
 
-                return result;
-            });
-        } catch (error) {
-            LoggerUtil.error('결제 생성 오류', error, { dto });
-            throw error;
-        }
-    }
+    //             return result;
+    //         });
+    //     } catch (error) {
+    //         LoggerUtil.error('결제 생성 오류', error, { dto });
+    //         throw error;
+    //     }
+    // }
 
     // 분산 락
-    async createPaymentWithDistributedLock(dto: FacadeCreatePaymentDto): Promise<PrismaPayment> {
+    async createPayment(dto: FacadeCreatePaymentDto): Promise<PrismaPayment> {
         const facadeCreatePaymentDto = plainToInstance(FacadeCreatePaymentDto, dto);
 
         const errors = await validate(facadeCreatePaymentDto);
